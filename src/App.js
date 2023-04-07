a0655948505d6c800b0eb0ab2e2c55aa6d5f6b04
Vishwanath Krishnappanavar <vi20365947@wipro.com>
2023-04-08 00:27:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const getProducts = useCallback(async () => {
+    const { data } = await axios.get(
+      "https://dummyjson.com/products?limit=100"
+    );
+    setProducts(data.products);
+    console.log("dhata", data);
+  }, []);
+
+  useEffect(() => {
+    getProducts();
+  }, [getProducts]);
+  const getPageNumbers = useCallback(() => {
+    const pageNumbers = Number.isInteger(products.length / 10)
+      ? products.length / 10
+      : parseInt(products.length / 10) + 1;
+    return pageNumbers;
+  }, [products.length]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="products">
+        {products.slice(page * 10 - 10, page * 10).map((product) => {
+          return (
+            <div className="product" key={product.id}>
+              <img src={product.images[0]} alt={product.title} />
+              <b>{product.title}</b>
+            </div>
+          );
+        })}
+      </div>
+      {products.length > 10 && (
+        <div className="pagination">
+          {page !== 1 && <span onClick={() => setPage(page - 1)}>◀</span>}
+          {Array(getPageNumbers())
+            .fill()
+            .map((_, i) => i + 1)
+            .map((item, i) => (
+              <span
+                className={page === item ? "pagination_selected" : ""}
+                onClick={() => setPage(item)}
+                key={i}
+              >
+                {item}
+              </span>
+            ))}
+          {page !== 10 && <span onClick={() => setPage(page + 1)}>▶</span>}
+        </div>
+      )}
     </div>
   );
 }
